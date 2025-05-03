2786fbf3edb98a8c61905af039156719c1d13a53
Rishi Raghuvanshi <rishisrmscet@gmail.com>
2025-05-03 22:09:39 +0530

    done!!

---
+++ String/findLen.js
@@ -0,0 +1,13 @@
+const s = "abc";
+
+// function findLen(str) {
+//   let len = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     len++;
+//   }
+//   return len;
+// }
+
+// console.log(findLen(s));
+
+console.log(s.length)
