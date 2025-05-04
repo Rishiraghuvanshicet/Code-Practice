ee3e42ff564b956c408999a8bc61093502d7870f
Rishi Raghuvanshi <rishisrmscet@gmail.com>
2025-05-04 22:36:24 +0530

    done!!

---
+++ String/twoStrAreSame.js
@@ -0,0 +1,17 @@
+const str1 = "abc";
+const str2 = "abc";
+
+function checkTwoStrAreSame(s1, s2) {
+  const n1 = s1.length - 1;
+  const n2 = s2.length - 1;
+
+  for (let i = 0; i < n1; i++) {
+    if (s1[i] !== s2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const value = checkTwoStrAreSame(str1, str2) ? "same" : "not Same";
+console.log(value);
