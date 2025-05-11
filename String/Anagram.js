dc35459eace16566236d0e8af3a1ccfdae90b338
Rishi Raghuvanshi <rishisrmscet@gmail.com>
2025-05-11 12:49:08 +0530

    done!!

---
+++ String/Anagram.js
@@ -0,0 +1,25 @@
+const s1 = "geeks";
+const s2 = "kseeg";
+
+function anagram(str1, str2) {
+  const newArray = new Array(26).fill(0);
+
+  for (let i of str1) {
+    let el = i.charCodeAt(0) - "a".charCodeAt(0);
+    newArray[el] += 1;
+  }
+
+  for (let z of str2) {
+    let el = z.charCodeAt(0) - "a".charCodeAt(0);
+    newArray[el] -= 1;
+  }
+  
+  for (let y of newArray) {
+    if (y !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(anagram(s1, s2));
